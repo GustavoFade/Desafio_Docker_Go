5f7d7ba7f1f5702732b479a9a9681d85fac85d2c
Gustavo Fadel <gustavo-959694@hotmail.com>
2023-08-03 22:26:53 -0300

    Finalizado desafio Go

---
+++ src/hello.go
@@ -0,0 +1,7 @@
+package main
+// https://hub.docker.com/layers/gustaofadel/fullcycle/golang/images/sha256-3a3f63e2f94a31a88d0414ce475fa169f56556ea5bf280efadabce0591299ad7?tab=layers
+import "fmt"
+
+func main() {
+    fmt.Println("Full Cycle Rocks!!")
+}
